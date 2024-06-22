06e3761c4b8207ba8f45503a7788a0f8e2d20596
Gabriel <gabrielnavarro.vip@gmail.com>
2024-06-21 22:24:39 -0300

    feat: add animation in hamburguer icon.

--- src/components/navbarMenu/NavbarMenu.tsx
+++ src/components/navbarMenu/NavbarMenu.tsx
@@ -6,7 +6,6 @@ import nightIcon from "../../assets/svg/night_icon.svg";
 import dayIcon from "../../assets/svg/day_icon.svg";
 
 import { Link } from "react-router-dom";
-import { TbMenu2 } from "react-icons/tb";
 import { Navbar } from "./NavbarMenu.styles";
 
 export default function NavbarMenu() {
@@ -38,16 +37,32 @@ export default function NavbarMenu() {
         </span>
         <div>
           <button onClick={toggleTheme}>
-            {theme === "light" ? <img src={dayIcon} alt="" /> : <img src={nightIcon} alt="Icone do darkMode" />}
+            {theme === "light" ? (
+              <img src={dayIcon} alt="" />
+            ) : (
+              <img src={nightIcon} alt="Icone do darkMode" />
+            )}
           </button>
         </div>
       </div>
 
       <div className="navbar-mobile">
-      <button onClick={toggleTheme}>
-        {theme === "light" ? <img src={dayIcon} alt="" /> : <img src={nightIcon} alt="Icone do darkMode" />}
+        <button onClick={toggleTheme}>
+          {theme === "light" ? (
+            <img src={dayIcon} alt="" />
+          ) : (
+            <img src={nightIcon} alt="Icone do darkMode" />
+          )}
         </button>
-        <span><TbMenu2 /></span> 
+
+        <div id="menuToggle">
+          <input id="checkbox" type="checkbox" />
+          <label className="toggle" htmlFor="checkbox">
+            <div className="bar bar--top"></div>
+            <div className="bar bar--middle"></div>
+            <div className="bar bar--bottom"></div>
+          </label>
+        </div>
       </div>
     </Navbar>
   );
--- src/pages/singUp/SingUp1.tsx
+++ src/pages/singUp/SingUp1.tsx
@@ -5,74 +5,68 @@ import { ContentButtons, ContentInputs } from "./SingUp.styles";
 import Button from "../../components/button/Button";
 import NavbarMenu from "../../components/navbarMenu/NavbarMenu";
 
-export default function SingUp1(){
+export default function SingUp1() {
+  const inputFields = [
+    {
+      label: "Nome*",
+      placeholder: "Nome",
+      type: "text",
+      icon: "User" as "User",
+    },
+    {
+      label: "Sobrenome*",
+      placeholder: "Sobrenome",
+      type: "text",
+      icon: "User" as "User",
+    },
+    {
+      label: "Email*",
+      placeholder: "exemplo@exemplo.com.br",
+      type: "email",
+      icon: "Email" as "Email",
+    },
+    {
+      label: "Senha*",
+      placeholder: "No mínimo 6 caracteres",
+      type: "password",
+      icon: "Password" as "Password",
+    },
+    {
+      label: "Confirme sua senha*",
+      placeholder: "Confirme sua senha",
+      type: "password",
+      icon: "Password" as "Password",
+    },
+  ];
 
-    const inputFields = [
-        {
-            label: 'Nome*',
-            placeholder: 'Nome',
-            type: 'text',
-            icon: 'User' as 'User'
-        },
-        {
-            label: 'Sobrenome*',
-            placeholder: 'Sobrenome',
-            type: 'text',
-            icon: 'User' as 'User'
-        },
-        {
-            label: 'Email*',
-            placeholder: 'exemplo@exemplo.com.br',
-            type: 'email',
-            icon: 'Email' as 'Email'
-        },
-        {
-            label: 'Senha*',
-            placeholder: 'No mínimo 6 caracteres',
-            type: 'password',
-            icon: 'Password' as 'Password'
-        },
-        {
-            label: 'Confirme sua senha*',
-            placeholder: 'Confirme sua senha',
-            type: 'password',
-            icon: 'Password' as 'Password'
-        }
-    ]
+  return (
+    <div>
+      <NavbarMenu />
+      <LogoBichinhos />
+      <ContentInputs>
+        <h3>Crie sua conta</h3>
 
-    return (
-       <div>
-        <NavbarMenu />
-        <LogoBichinhos />
-        <ContentInputs>
-            <h3>Crie sua conta</h3>
+        {inputFields.map((field) => (
+          <InputField
+            key={field.label}
+            label={field.label}
+            placeHolder={field.placeholder}
+            type={field.type}
+            icon={field.icon}
+          />
+        ))}
+      </ContentInputs>
 
-            {inputFields.map((field) => (
-                <InputField 
-                    label={field.label} 
-                    placeHolder={field.placeholder} 
-                    type={field.type} 
-                    icon={field.icon} />
-            ))}
-        </ContentInputs>
+      <ContentButtons>
+        <Button
+          text="Voltar"
+          color="rgba(68, 68, 68, 0.14)"
+          textColor="#F86B01"
+          width={15}
+        />
 
-        <ContentButtons>
-            <Button 
-            text="Voltar" 
-            color="rgba(68, 68, 68, 0.14)" 
-            textColor="#F86B01" 
-            width={15} 
-            
-            />
-            
-            <Button 
-            text="Próximo" 
-            color="#F86B01" 
-            textColor="#312E38" 
-            width={15} 
-            />
-
-        </ContentButtons>
-       </div>
-    )
+        <Button text="Próximo" color="#F86B01" textColor="#312E38" width={15} />
+      </ContentButtons>
+    </div>
+  );
 }
