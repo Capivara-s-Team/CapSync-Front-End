66270712e8f4fbbc2bfb41fc292afa095e05aafe
pierresouza <pierre.s3@hotmail.com>
2024-06-28 16:09:20 -0300

    :sparkles: chore: update and add new areas

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/home/Home";
 import { Login } from "./pages/login/Login";
-import Profile from "./pages/profile/Profile";
-import Error from "./pages/error/ErrorPage";
-import ListVolunteers from "./pages/listVolunteers/ListVolunteers";
-import PasswordRecovery from "./pages/passwordRecovery/PasswordRecovery";
 import SignUp1 from "./pages/signUp1/SignUp1";
 import SignUp2 from "./pages/signUp2/SignUp2";
-
-import HomeLogged from "./pages/homeLogged/HomeLogged";
-
 import SignUp3 from "./pages/signUp3/SignUp3";
 
+import HomeLogged from "./pages/homeLogged/HomeLogged";
+import PasswordRecovery from "./pages/passwordRecovery/PasswordRecovery";
+import Profile from "./pages/profile/Profile";
+import { VolunteerProfile } from "./pages/VolunteerProfile/VolunteerProfile";
+import ListVolunteers from "./pages/listVolunteers/ListVolunteers";
+import Error from "./pages/error/ErrorPage";
 
 export default function App() {
   return (
@@ -22,13 +21,12 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup1" element={<SignUp1 />} />
         <Route path="/signup2" element={<SignUp2 />} />
-
-        <Route path="/homelogged" element={<HomeLogged />} />
-
         <Route path="/signup3" element={<SignUp3 />} />
 
+        <Route path="/homelogged" element={<HomeLogged />} />
         <Route path="/recovery" element={<PasswordRecovery />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/volunteer-profile" element={<VolunteerProfile />} />
 
         <Route path="/list" element={<ListVolunteers />} />
         <Route path="/*" element={<Error />} />
