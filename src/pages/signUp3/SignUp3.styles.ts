7c61b2e8fa062d5e2632ece3bf590d80c6798628
pierresouza <pierre.s3@hotmail.com>
2024-06-27 22:16:53 -0300

    :sparkles: feat: create signup3

--- src/pages/signUp3/SignUp3.styles.ts
+++ src/pages/signUp3/SignUp3.styles.ts
@@ -10,7 +10,6 @@ export const Content = styled.div`
 width: 100%;
 height: 100%;
 gap: 1rem;
-row-gap: 1rem;
 
 display: flex;
 align-items: center;
@@ -18,7 +17,8 @@ justify-content: space-evenly;
 
 .Card-content{
   max-width: 610px;
-  width: 100%;
+  gap: 1.5rem;
+  padding: 56px ;
 
   span{
     font-size: 1rem;
@@ -28,6 +28,41 @@ justify-content: space-evenly;
 }
 
 `;
+
+export const Deficient = styled.div`
+display: flex;
+gap: 32px;
+
+.check_option{
+  display: flex;
+  flex-direction: column;
+  height: 20px;
+  gap: 1rem;
+
+  label{
+    white-space: nowrap;
+    display: flex;
+  }
+}
+
+.box-checkbox{
+  display: flex;
+  justify-content: space-evenly;
+
+  input[type="checkbox"]{
+    width: 20px;
+    height: 20px;
+
+  }
+
+}
+`;
+
+export const Gender = styled.div`
+  display: flex;
+  gap: 2rem;
+`;
+
 export const ContentInputs = styled.div`
   width: 100%;
   height: 100%;
@@ -35,15 +70,21 @@ export const ContentInputs = styled.div`
   flex-direction: column;
 
   form{
-    border: 1px solid #ff0000;
     width: 100%;
+    height: 80%;
     display: flex;
     align-items: center;
     justify-content: center;
-    gap: 1rem;
+    flex-direction: column;
+    gap:2rem;
+
+    label{
+      color: #FFF;
+      padding-bottom: 12px;
+    }
+
     input{
-      width: 220px;
-      border: 1px solid #ff0000;
+      width: 216px;
   }
   }
 `;
--- src/pages/signUp3/SignUp3.tsx
+++ src/pages/signUp3/SignUp3.tsx
@@ -4,12 +4,7 @@ import mainLogo from "../../assets/svg/main_logo.svg";
 
 import Navbar from "../../components/navbarMenu/NavbarMenu";
 
-import {
-  Container,
-  Content,
-  ContentButtons,
-  ContentInputs,
-} from "./SignUp3.styles";
+import { Container, Content, Gender, ContentButtons, ContentInputs, Deficient } from "./SignUp3.styles";
 import { Link } from "react-router-dom";
 import Button from "../../components/button/Button";
 import CardForms from "../../components/cardForms/CardForms";
@@ -27,42 +22,48 @@ export default function SignUp2() {
         </div>
         <CardForms text="Crie sua conta" className="Card-content">
           <span>
-            O propósito das próximas perguntas é coletar os dados de inclusão
-            dos nossos voluntários para tornar a comunidade cada vez mais
-            diversa. As respostas não influenciam na sua aceitação na
+            O propósito das próximas perguntas é coletar os dados de inclusão dos nossos voluntários para tornar a comunidade cada vez mais diversa. As respostas não influenciam na sua aceitação na
             comunidade.
           </span>
           <Formik initialValues={{}} onSubmit={() => {}}>
             <ContentInputs>
               <Form>
-                <div>
-                  <label htmlFor="">Autodeclaração étnico-racial</label>
-                  <InputField id="a" type="text" name="teste" placeHolder="" />
-                </div>
-                <div>
-                  <label htmlFor="">Gênero</label>
-                  <InputField id="a" type="text" name="teste" placeHolder="" />
-                </div>
+                <Gender>
+                  <div>
+                    <label htmlFor="">Autodeclaração étnico-racial</label>
+                    <InputField id="a" type="text" name="teste" placeHolder="" />
+                  </div>
+                  <div>
+                    <label htmlFor="">Gênero</label>
+                    <InputField id="a" type="text" name="teste" placeHolder="" />
+                  </div>
+                </Gender>
+                <Deficient>
+                  <div className="check_option">
+                    <div>
+                      <label htmlFor="">Você possui alguma deficiência?</label>
+                    </div>
+                    <div className="box-checkbox">
+                      <input type="checkbox" name="" id="" />
+                      <label htmlFor="">Sim</label>
+                      <input type="checkbox" name="" id="" />
+                      <label htmlFor="">Não</label>
+                    </div>
+                  </div>
+                  <div>
+                    <label htmlFor="">Autodeclaração étnico-racial</label>
+                    <InputField id="a" type="text" name="teste" placeHolder="" />
+                  </div>
+                </Deficient>
               </Form>
             </ContentInputs>
           </Formik>
           <ContentButtons>
             <Link to={"/signup1"}>
-              <Button
-                text="Voltar"
-                color="rgba(68, 68, 68, 0.14)"
-                textColor="#F86B01"
-                width={15}
-              />
+              <Button text="Voltar" color="rgba(68, 68, 68, 0.14)" textColor="#F86B01" width={15} />
             </Link>
             <Link to={"/signup3"}>
-              <Button
-                text="Próximo"
-                color="#F86B01"
-                textColor="#312E38"
-                width={15}
-                icon={true}
-              />
+              <Button text="Cadastrar" color="#F86B01" textColor="#312E38" width={15} icon={true} />
             </Link>
           </ContentButtons>
         </CardForms>
